ff70ffe1fafbfd69d51cc5fd61a922570f149745
Pampaj <leonardo.pamaploni666@gmail.com>
2023-09-30 16:46:18 +0200

    Initial commit

---
+++ ScikitTry.py
@@ -0,0 +1,10 @@
+from sklearn.ensemble import RandomForestClassifier
+
+clf = RandomForestClassifier(random_state=0)
+X = [[1, 2, 3],  # 2 samples, 3 features
+     [11, 12, 13]]
+y = [0, 1]  # classes of each sample
+clf.fit(X, y)  # estimator
+
+clf.predict(X)  # predict classes of the training data
+clf.predict([[4, 5, 6], [14, 15, 16]])  # predict classes of new data
